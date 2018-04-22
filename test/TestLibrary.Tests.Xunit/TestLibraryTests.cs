5b7f36e47eb7c9c4fc2e8e4feb77c3b47850a944
Zoltán Tóth <zshun86@gmail.com>
2018-04-23 00:44:52 +0200

    xUnit test project added.

---
+++ test/TestLibrary.Tests.Xunit/TestLibraryTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace TestLibrary.Tests.Xunit
+{
+    public class TestLibraryTests
+    {
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(3, 4, 7)]
+        public void Two_plus_two_should_be_four(int first, int second, int result)
+        {
+            var subject = new TestLibrary();
+            Assert.Equal(result, subject.Add(first, second));
+        }
+
+        [Fact]
+        public void TernaryIfTest()
+        {
+            var subject = new TestLibrary();
+            Assert.Equal(0, subject.TernaryIfTest(2, 3));
+        }
+
+        [Fact]
+        public async Task TaskTest()
+        {
+            var subject = new TaskTest();
+            var elapsedTime = await subject.Run();
+            Assert.True(elapsedTime < 9000);
+        }
+    }
+}
